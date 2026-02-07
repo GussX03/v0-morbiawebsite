89308533cd49aac9e4ffedcfff2851701adb1797
Gustavo Morales <62187656+GussX03@users.noreply.github.com>
2026-02-07 15:02:06 -0600

    Replace Google Tag Manager with Google Analytics
    Removed Google Tag Manager script and added Google Analytics tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type React from "react"
 import type { Metadata } from "next"
-import Script from "next/script"
 import "./globals.css"
 
 export const metadata: Metadata = {
@@ -24,33 +23,17 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XF3NM0ZLF8"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-XF3NM0ZLF8');
+    </script>
     <html lang="en">
-      <body>
-        {/* Google Tag Manager */}
-        <Script
-          id="google-tag-manager"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-XXXXXXX');
-            `,
-          }}
-        />
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
