6d0b9bf63975cfe7a694fec083fd2be7bceb866c
Gustavo Morales <62187656+GussX03@users.noreply.github.com>
2026-02-07 15:03:42 -0600

    Remove Google Analytics tracking script
    Removed Google Analytics tracking script from layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,15 +23,6 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XF3NM0ZLF8"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-XF3NM0ZLF8');
-    </script>
     <html lang="en">
       <body>{children}</body>
     </html>
